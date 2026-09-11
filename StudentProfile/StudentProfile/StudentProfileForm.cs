54ac1ed1594300dc53683dc02fcc80614716bdda
JM Justine <jmandjustine@outlook.com>
2026-09-11 17:41:47 +0800

    Add StudentProfile Visual Studio project

---
+++ StudentProfile/StudentProfile/StudentProfileForm.cs
@@ -0,0 +1,15 @@
+namespace StudentProfile
+{
+    public partial class StudentProfileForm : Form
+    {
+        public StudentProfileForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Von Venzon");
+        }
+    }
+}
